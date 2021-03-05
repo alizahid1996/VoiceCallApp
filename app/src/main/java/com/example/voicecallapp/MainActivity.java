f858b64ff43f17e0f8c10bceb49e56fc60912838
Ali Zahid <ali.zahid050@gmail.com>
2021-03-05 12:34:52 +0500

    commit 100 push ali

--- app/src/main/java/com/example/voicecallapp/MainActivity.java
+++ app/src/main/java/com/example/voicecallapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.voicecallapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.voicecallapp.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    //SinchClient sinchClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+
     }
 }
